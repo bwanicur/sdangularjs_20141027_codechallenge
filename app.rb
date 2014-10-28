a1845e9b4bc7363af47fb964eeaeddacb5f93c1c
ben wanicur <bwanicur@gmail.com>
2014-10-27 18:54:24 -0700

    allow all http verbs

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ enable :logging, :dump_errors, :raise_errors unless ENV['RACK_ENV'] == 'producti
 
 before do
   headers 'Access-Control-Allow-Origin' => '*', 
-          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']  
+          'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST', 'PATCH', 'PUT']  
   halt(403, 'Not Authorized') unless params[:token] == 'SD-AngularJS'
 end
 
